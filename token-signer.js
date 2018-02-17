c84490440dd454977e765a8b81857d5b774f9147
Jack Cable <jackhcable@gmail.com>
2018-02-17 00:05:00 -0700

    Add crypto functionality

--- token-signer.js
+++ token-signer.js
@@ -1,8 +1,14 @@
 var util = require('util');
 var events = require('events');
+var crypto = require('crypto');
+var fs = require('fs');
+
+var private_key = fs.readFileSync('keys/privkey.pem', 'utf-8');
+var public_key = fs.readFileSync('keys/pubkey.pem', 'utf-8');
+var signer = crypto.createSign('sha256');
 
 var ResponseCodes = {
-  SUCCESS:    0,
+  SUCCESS: 0,
   FAILURE: 1
 };
 
@@ -15,6 +21,20 @@ util.inherits(TokenSigner, events.EventEmitter);
 TokenSigner.prototype.signToken = function(token) {
   // insert logic to sign token
   console.log('Token received: ' + token)
+
+  // Message Format
+  // For now: 'token' + '|' + 'timestamp'
+
+  var timestamp = Math.floor(new Date() / 1000)
+  var message = token + '|' + timestamp;
+
+  signer.update(message);
+  signer.end();
+
+  const signature = signer.sign(private_key)
+  const signature_hex = signature.toString('hex')
+  console.log('Signed message: ' + message);
+  console.log('Generated signature: ' + signature_hex);
   this.emit('ready', ResponseCodes.ResponseCodes);
 };
 
